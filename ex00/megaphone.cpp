f0ffb5d16dc196e31606674e5b02c5dd9d01842e
ELkhalil <elkhalilabdrahman@gmail.com>
2023-03-28 16:18:40 +0000

    hpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,31 @@
 /*   By: aelkhali <aelkhali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 13:02:58 by aelkhali          #+#    #+#             */
-/*   Updated: 2023/03/28 13:02:59 by aelkhali         ###   ########.fr       */
+/*   Updated: 2023/03/28 15:46:23 by aelkhali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+int main(int ac, char **av) 
+{
+	if (ac < 2) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+
+	for (int i = 1; i < ac; ++i) {
+		for (int j = 0; j < (int)strlen(av[i]); ++j) {
+			char c = av[i][j];
+			if (islower(c))
+				c = toupper(c);
+			std::cout << c;
+		}
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
+ 
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelkhali <aelkhali@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 15:55:31 by aelkhali          #+#    #+#             */
+/*   Updated: 2023/03/28 16:12:03 by aelkhali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK
+# define PHONEBOOK
+
+class PhoneBook {
+
+public:
+    PhoneBook(void);
+    ~PhoneBook(void);
+};
+
+#endif
