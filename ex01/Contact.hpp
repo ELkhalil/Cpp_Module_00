8065605db8f500968d5f49c77fe5e2fd38b5012a
ELkhalil <elkhalilabdrahman@gmail.com>
2023-05-13 17:09:49 +0100

    before freez

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,12 +6,14 @@
 /*   By: aelkhali <aelkhali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 15:56:03 by aelkhali          #+#    #+#             */
-/*   Updated: 2023/04/04 14:59:37 by aelkhali         ###   ########.fr       */
+/*   Updated: 2023/04/04 22:33:37 by aelkhali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
+#include <iostream>
+#include <string>
 
 class Contact {
 
